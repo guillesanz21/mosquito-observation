b78214adbf17d9f2612398045a6b60f3be0fdc39
guillesanz21 <guillermo.sanz.gonzalez@alumnos.upm.es>
2025-02-06 19:28:04 +0100

    Filter by status

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import React from 'react'
 import MyTable from '../components/MyTable'
+import { Form } from 'react-bootstrap'
+import { createLogicalAnd } from 'typescript'
 
 type Props = {}
 export type Observation = {
@@ -15,6 +17,7 @@ export type Observation = {
 
 const Home = (props: Props) => {
     const [observations, setObservations] = React.useState([] as Observation[])
+    const [statusFilter, setStatusFilter] = React.useState('')
     const [idToUpdate, setIdToUpdate] = React.useState(null as number | null)
 
     const apiUrl = process.env.REACT_APP_API_URL;
@@ -22,17 +25,35 @@ const Home = (props: Props) => {
 
     React.useEffect(() => {
         const fetchObservations = async () => {
-            const { data } = await axios.get(apiObservationsUrl);
+            let filterQuery = statusFilter !== "" ? `?status=${statusFilter}` : '';
+            console.log(`${apiObservationsUrl}${filterQuery}`)
+            const { data } = await axios.get(`${apiObservationsUrl}${filterQuery}`);
             setObservations(data);
         };
 
         fetchObservations();
-    }, []);
+    }, [statusFilter]);
+
+
+    const filterByStatus = async (status: string) => {
+        setStatusFilter(status);
+    }
 
     return (
         <>
             <div id="section">
                 <h2>Mosquito Observations</h2>
+
+                <div className="filter">
+                    <p>Do you want to filter by status:</p>
+                    <Form.Select aria-label="Default select example" onChange={(e) => filterByStatus(e.target.value)}>
+                        <option value="">Open this select menu</option>
+                        <option value="pending">Pending</option>
+                        <option value="in-progress">In progress</option>
+                        <option value="complete">Complete</option>
+                    </Form.Select>
+
+                </div>
                 <MyTable observations={observations} />
             </div>
         </>
