110ec29c7f8594eb4b9af906239604df6bc475e1
guillesanz21 <guillermo.sanz.gonzalez@alumnos.upm.es>
2025-02-06 18:52:06 +0100

    test connection with back - FAIL CORS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // Fetch data from the API
+  const url = process.env.REACT_APP_API_URL;
+  const get_observations = `${url}/api/observations/`;
+  React.useEffect(() => {
+    const fetchObservations = async () => {
+      const response = await fetch(get_observations);
+      const data = await response.json();
+      console.log(data);
+    };
+    fetchObservations();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
